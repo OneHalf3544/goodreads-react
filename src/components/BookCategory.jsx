fe5df73322f4dca88e0f170bc49edda2d5dc7a48
Mikhail.Sergeyev <mikhail.sergeyev@nexign-systems.com>
2021-01-05 02:03:55 +0300

    test: add tests to book categories

--- src/components/BookCategory.jsx
+++ src/components/BookCategory.jsx
@@ -3,16 +3,21 @@ import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {ADD_BOOK_ACTION, DELETE_BOOK_ACTION} from "../state/actions";
 
+const BookItem= ({ book, deleteBook}) =>
+    (<li className="list-group-item d-flex p-1" key={book.title}>
+        <div className="flex-grow-1">{book.title}</div>
+        <button className="btn btn-danger" onClick={e => deleteBook(book)}>Delete</button>
+    </li>)
+
 const BookCategory = ({name, books, addBook, deleteBook}) => {
     const [newName, setNewName] = useState("")
 
     return (<div className="card">
         <h2 className="card-header">{name}</h2>
         <ul className="card-body list-group p-0">
-            {books.map(book => <li className="list-group-item d-flex p-1" key={book.title}>
-                <div className="flex-grow-1">{book.title}</div>
-                <button className="btn btn-danger" onClick={e => deleteBook(book)}>Delete</button>
-            </li>)}
+            {books.map(book =>
+                <BookItem book={book} deleteBook={deleteBook} />
+                )}
         </ul>
         <div className="card-footer d-flex flex-row p-1">
             <input className="flex-grow-1" inputMode="text" value={newName} onChange={e => setNewName(e.target.value)} />
